502bdf153942b7793d81ab702f70a2db3720a259
Margie Ball <margie_ball@glic.com>
2021-07-05 23:08:43 -0400

    added profile component

--- finalproject-frontend/src/App.js
+++ finalproject-frontend/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import{Link, Route} from "react-router-dom";
 import Home from './components/Home';
 import SignUp from './components/SignUp';
+import Login from './components/Login';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
       <Route path="/" exact render={() => <Home /> } />
       <Route path="/signup" render={(props) => <SignUp {...props} /> } />
       <Route path="/login" render={(props) => <Login {...props} /> } />
+      <Route path="/profile/:id" render={(props) => <UserProfile {...props} /> } />
     </div>
   );
 }
---
+++ finalproject-frontend/src/components/UserProfile.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+
+class UserProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                username: "",
+                password: "",
+                email: "",
+                jwStatus: "",
+                topic: ""
+            }
+        }
+    }
+
+    componentDidMount = () => {
+        axios.get(`http://localhost:3001/users/profile/${this.props.match.params.id}`)
+        .then(response => {
+            console.log(response)
+            this.setState({
+                user: response.data.user
+            })
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState(prevState => ({
+            user: {
+                ...prevState.user,
+                [event.target.name]: event.target.value
+            }
+        }))
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        axios.put(`http://localhost:3001/users/profile/${this.props.match.params.id}`)
+            .then(response => 
+                console.log("Making Changes"))
+    }
+
+    handleDelete = async (event) => {
+        await axios.delete(`http://localhost:3001/users/profile/${this.props.match.params.id}`)
+            .then(response => {
+                console.log("deleting user")
+                this.props.history.push('/')
+            })
+    }
+
+    render() {
+        const user = this.state.user;
+
+        return (
+            <div>
+                <div>Navigation</div>
+                <h1>Profile</h1>
+                <form>
+                    <h2>Username: {user.username}</h2>
+                    <h2>Password: {user.password}</h2>
+                    <h2>Email: {user.email}</h2>
+                    <h2>JW Status: {user.jwStatus}</h2>
+                    <h2>Topic to Discuss: {user.topic}</h2>
+                </form>
+                <div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserProfile;
