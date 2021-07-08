38d8f4199bb77f95069abc4b5d3ac542f1595480
Margie Ball <margie_ball@glic.com>
2021-07-08 12:01:56 -0400

    added resource pages

---
+++ finalproject-frontend/src/components/Glossary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link, Route} from "react-router-dom";
+
+const Glossary = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Glossary;
---
+++ finalproject-frontend/src/components/Statistics.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link, Route} from "react-router-dom";
+
+const Statistics = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Statistics;
