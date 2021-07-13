8d3ce2f8ce6aa935542decbbec864857dac5a5ab
Margie Ball <margie_ball@glic.com>
2021-07-13 17:46:26 -0400

    added mentalhealth content

--- finalproject-frontend/src/components/DeProgram.js
+++ finalproject-frontend/src/components/DeProgram.js
@@ -24,22 +24,52 @@ const DeProgram = () => {
                 </div>
             </div>
             <h1>De-Programming Resources</h1>
-            <ul>YouTube Channels
+            <p>These resources can help you start to reclaim your mind and confirm why leaving the Jehovah's Witnesses is the right decision. </p>
+
+            <ul><h3>YouTube Channels</h3>
                 <li>Lloyd Evans</li>
                 <li>Telltale</li>
                 <li>Mentally Diseased</li>
+                <li>ExJW Critical Thinker</li>
+                <li>Jay the Comedian</li>
             </ul>
-            <ul>Books
-                <li>The Reluctant Apostate by Lloyd Evans</li>
-                <li>Crisis of Conscience by Raymond Franz</li>
-                <li>In Search of Christian Freedom by Raymond Franz</li>
-                <li>The Gentile Times Reconsidered by Carl O. Johnson</li>
-                <li>Exiting the JW Cult: A Healing Handbook by Bonnie Zieman</li>
-                <li>Shunned: A Survival Guide by Bonnie Zieman</li>
-                <li>Cracking the Cult Code for Therapists: What Every Cult Victim Wants Their Therapist to Know by Bonnie Zieman</li>
+
+            <ul><h3>TV Programs</h3>
+                <li><em>Seduced: Inside the NXIVM Cult</em></li>
+                <li><em>The Vow</em></li>
+                <li><em>Leah Remini: Scientology and the Aftermath</em> - Season 3 Special</li>
+                <li><em>Cults and Extreme Belief</em> - Episode 2</li>
+                <li><em>The Witnesses</em></li>
             </ul>
-            <ul>Podcasts
-                <li>Shunned</li>
+
+            <ul><h3>Movies</h3>
+                <li><em>Apostasy (2017)</em> Directed by Daniel Kokotajlo</li>
+                <li><em>Worlds Apart (2008)</em> Directed by Niels Arden Oplev</li>
+                <li><em>The Children Act (2018)</em> Directed by Richard Evre</li>
+                <li><em>Worldly Girl (2016)</em> Directed by Marco Danieli</li>
+            </ul>
+
+            <ul><h3>Websites</h3>
+                <li><a href={"https://jwwatch.org/"}>JW Watch</a></li>
+                <li><a href={"https://jwfacts.com/"}>JW Facts</a></li>
+                <li><a href={"https://jwleaks.org/"}>JW Leaks</a></li>
+            </ul>
+
+            <ul><h3>Books</h3>
+                <li><em>The Reluctant Apostate</em> by Lloyd Evans</li>
+                <li><em>Crisis of Conscience</em> by Raymond Franz</li>
+                <li><em>In Search of Christian Freedom</em> by Raymond Franz</li>
+                <li><em>The Gentile Times Reconsidered</em> by Carl O. Johnson</li>
+                <li><em>Leaving the Witness:Exiting a Religion and Finding a Life</em> by Amber Scorah</li>
+                <li><em>Exiting the JW Cult: A Healing Handbook</em> by Bonnie Zieman</li>
+                <li><em>Shunned: A Survival Guide</em> by Bonnie Zieman</li>
+                <li><em>Cracking the Cult Code for Therapists: What Every Cult Victim Wants Their Therapist to Know</em> by Bonnie Zieman</li>
+            </ul>
+            <ul><h3>Podcasts</h3>
+                <li><em>This JW Life</em></li>
+                <li><em>Ex-Jehovah's Witnesses-Critical Thinkers</em></li>
+                <li><em>Shunned</em></li>
+                <li><em>The Uncultured Podcast</em></li>
             </ul>
         </div>
 
--- finalproject-frontend/src/components/MentalHealth.js
+++ finalproject-frontend/src/components/MentalHealth.js
@@ -24,24 +24,39 @@ const MentalHealth = () => {
                 </div>
             </div>
             <h1>Mental Health Resources</h1>
-            <ul>Online Resources
+
+            <ul><h3>General Mental Health Resources</h3>
                 <li><a href={"https://www.nimh.nih.gov/health/"}>National Institute of Mental Health</a></li>
                 <li><a href={"https://www.mentalhealth.gov/"}>MentalHealth.gov</a></li>
             </ul>
-            <ul>Suicide Prevention
+
+            <ul><h3>Suicide Prevention</h3>
                 <li><a href={"https://metanoia.org/suicide"}>Metanoia</a></li>
                 <li><a href={"https://suicidepreventionlifeline.org"}>Suicide Prevention Lifeline</a></li>
                 <li><a href={"http://crisischat.org"}>Life Crisis Chat</a></li>
                 <li>National Suicide Prevention Hotline: 1-800-273-8255</li>
             </ul>
-            <ul>Books
-                <li>Combatting Cult Mind Control by Steven Hassan</li>
-                <li>The Psychology of Religion: An Empirical Approach by</li>
+
+            <ul><h3>Books</h3>
+                <li><em>Combatting Cult Mind Control</em> by Steven Hassan</li>
+                <li><em>The Psychology of Religion: An Empirical Approach</em> by Barnard Spilka</li>
+                <li><em>Exiting the JW Cult: A Healing Handbook</em> by Bonnie Zieman</li>
+                <li><em>Cracking the Cult Code for Therapists:What Every Cult Victim Wants Their Therapist to Know</em> by Bonnie Zieman</li>
             </ul>
-            <ul>Counseling
+
+            <ul><h3>Counseling Resources</h3>
                 <li><a href={"https://www.recoveringfromreligion.org/#rfr-welcome"}>Recovering from Religion</a></li>
                 <li><a href={"https://culteducation.com/directory-of-cult-recovery-resources.htm"}>Cult Education</a></li>
             </ul>
+
+            <ul><h3>Meditation YouTube Channels</h3>
+                <li>Goodful</li>
+                <li>Calm</li>
+                <li>Tara Brach</li>
+                <li>Great Meditation</li>
+                <li>Headspace</li>
+            </ul>
+
         </div>
     )
 }
