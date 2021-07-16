84a50ecfba10fe6d37468b7cca38a3e91aeb8d5c
Margie Ball <margie_ball@glic.com>
2021-07-15 23:36:28 -0400

    added images to beliefs

--- finalproject-frontend/src/components/Beliefs.js
+++ finalproject-frontend/src/components/Beliefs.js
@@ -25,16 +25,22 @@ const Beliefs = () => {
             <div className="belief-body main-body">
                 <div className="belief-content content">
                     <h1>Questionable Beliefs</h1>
-                    <p>There are many Watchtower beliefs that upon closer review are not scriptural or backed up by scientific or archeological evidence. Here are a few examples to review.</p>
-                    <ul className="belief-list">
-                        <li><a href={"http://www.jwfacts.com/watchtower/607-7-times.php"}>1914 Chronology</a></li>
-                        <li><a href={"http://www.jwfacts.com/watchtower/birthdays.php"}>Birthday Celebrations</a></li>
-                        <li><a href={"http://www.jwfacts.com/watchtower/blood-transfusions.php"}>Blood Transfusions</a></li>
-                        <li><a href={"http://www.jwfacts.com/watchtower/cross-or-stake.php"}>The Cross vs. Stake</a></li>
-                        <li><a href={"http://www.jwfacts.com/watchtower/144000.php"}>The 144,000</a></li>
-                        <li><a href={"http://www.jwfacts.com/watchtower/disfellowship-shunning.php"}>Disfellowshipping and Shunning</a></li>
-
-                    </ul>
+                    <p id="belief-header">There are many Watchtower beliefs that upon closer review are not scriptural or backed up by scientific or archeological evidence. Here are a few examples to review.</p>
+                    <div className="belief-container">
+                        <ul className="belief-list">
+                            <li><a href={"http://www.jwfacts.com/watchtower/607-7-times.php"}>1914 Chronology</a></li>
+                            <li><a href={"http://www.jwfacts.com/watchtower/birthdays.php"}>Birthday Celebrations</a></li>
+                            <li><a href={"http://www.jwfacts.com/watchtower/blood-transfusions.php"}>Blood Transfusions</a></li>
+                            <li><a href={"http://www.jwfacts.com/watchtower/cross-or-stake.php"}>The Cross vs. Stake</a></li>
+                            <li><a href={"http://www.jwfacts.com/watchtower/144000.php"}>The 144,000</a></li>
+                            <li><a href={"http://www.jwfacts.com/watchtower/disfellowship-shunning.php"}>Disfellowshipping and Shunning</a></li>
+                        </ul>
+                        <div className="belief-images">
+                            <img src="/images/noblood.jpg" alt="blood card" id="blood" />
+                            <img src="/images/birthday.jpg" alt="birthday cake" id="birthday" />
+                            <img src="/images/144000.jpg" alt="144000" id="144000" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
