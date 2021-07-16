b143a0a51ad16f03cf63dd4e2f4f6d8278d467e9
Margie Ball <margie_ball@glic.com>
2021-07-15 23:14:24 -0400

    added images

--- finalproject-frontend/src/components/DeProgram.js
+++ finalproject-frontend/src/components/DeProgram.js
@@ -27,7 +27,7 @@ const DeProgram = () => {
                     <h1>De-Programming Resources</h1>
                     <p id="deprogram-head">These resources can help you start to reclaim your mind and confirm why leaving the Jehovah's Witnesses is the right decision. </p>
 
-                    <div className="list-parent">
+                    <div className="list-parent deprogram-parent">
                         <div className="youtube">
                             <ul className="deprogram-list"><h3>YouTube Channels</h3>
                                 <li>Lloyd Evans</li>
@@ -39,21 +39,27 @@ const DeProgram = () => {
                             <img src="/images/youtube.png" alt="youtube" id="youtube"/>
                         </div>
                         
+                        <div className="tv">
+                            <ul className="deprogram-list"><h3>TV Programs</h3>
+                                <li><em>Seduced: Inside the NXIVM Cult</em></li>
+                                <li><em>The Vow</em></li>
+                                <li><em>Leah Remini: Scientology and the Aftermath</em> - Season 3 Special</li>
+                                <li><em>Cults and Extreme Belief</em> - Episode 2</li>
+                                <li><em>The Witnesses</em></li>
+                            </ul>
+                            <img src="/images/cult.jpg" alt="cults" id="tv" />
+                        </div>
 
-                        <ul className="deprogram-list"><h3>TV Programs</h3>
-                            <li><em>Seduced: Inside the NXIVM Cult</em></li>
-                            <li><em>The Vow</em></li>
-                            <li><em>Leah Remini: Scientology and the Aftermath</em> - Season 3 Special</li>
-                            <li><em>Cults and Extreme Belief</em> - Episode 2</li>
-                            <li><em>The Witnesses</em></li>
-                        </ul>
 
-                        <ul className="deprogram-list"><h3>Movies</h3>
-                            <li><em>Apostasy (2017)</em> Directed by Daniel Kokotajlo</li>
-                            <li><em>Worlds Apart (2008)</em> Directed by Niels Arden Oplev</li>
-                            <li><em>The Children Act (2018)</em> Directed by Richard Evre</li>
-                            <li><em>Worldly Girl (2016)</em> Directed by Marco Danieli</li>
-                        </ul>
+                        <div className="movies">
+                            <ul className="deprogram-list"><h3>Movies</h3>
+                                <li><em>Apostasy (2017)</em> Directed by Daniel Kokotajlo</li>
+                                <li><em>Worlds Apart (2008)</em> Directed by Niels Arden Oplev</li>
+                                <li><em>The Children Act (2018)</em> Directed by Richard Evre</li>
+                                <li><em>Worldly Girl (2016)</em> Directed by Marco Danieli</li>
+                            </ul>
+                            <img src="/images/apostasy.jpg" alt="apostasy movie" id="apostasy" />
+                        </div>
 
                         <ul className="deprogram-list"><h3>Websites</h3>
                             <li><a href={"https://jwwatch.org/"}>JW Watch</a></li>
@@ -61,16 +67,20 @@ const DeProgram = () => {
                             <li><a href={"https://jwleaks.org/"}>JW Leaks</a></li>
                         </ul>
 
-                        <ul className="deprogram-list"><h3>Books</h3>
-                            <li><em>The Reluctant Apostate</em> by Lloyd Evans</li>
-                            <li><em>Crisis of Conscience</em> by Raymond Franz</li>
-                            <li><em>In Search of Christian Freedom</em> by Raymond Franz</li>
-                            <li><em>The Gentile Times Reconsidered</em> by Carl O. Johnson</li>
-                            <li><em>Leaving the Witness:Exiting a Religion and Finding a Life</em> by Amber Scorah</li>
-                            <li><em>Exiting the JW Cult: A Healing Handbook</em> by Bonnie Zieman</li>
-                            <li><em>Shunned: A Survival Guide</em> by Bonnie Zieman</li>
-                            <li><em>Cracking the Cult Code for Therapists: What Every Cult Victim Wants Their Therapist to Know</em> by Bonnie Zieman</li>
-                        </ul>
+                        <div className="books">
+                            <ul className="deprogram-list"><h3>Books</h3>
+                                <li><em>The Reluctant Apostate</em> by Lloyd Evans</li>
+                                <li><em>Crisis of Conscience</em> by Raymond Franz</li>
+                                <li><em>In Search of Christian Freedom</em> by Raymond Franz</li>
+                                <li><em>The Gentile Times Reconsidered</em> by Carl O. Johnson</li>
+                                <li><em>Leaving the Witness:Exiting a Religion and Finding a Life</em> by Amber Scorah</li>
+                                <li><em>Exiting the JW Cult: A Healing Handbook</em> by Bonnie Zieman</li>
+                                <li><em>Shunned: A Survival Guide</em> by Bonnie Zieman</li>
+                                <li><em>Cracking the Cult Code for Therapists: What Every Cult Victim Wants Their Therapist to Know</em> by Bonnie Zieman</li>
+                            </ul>
+                            <img src="/images/lloyd.jpg" alt="lloyd evans" id="lloyd" />
+                        </div>
+
                         <ul className="deprogram-list"><h3>Podcasts</h3>
                             <li><em>This JW Life</em></li>
                             <li><em>Ex-Jehovah's Witnesses-Critical Thinkers</em></li>
